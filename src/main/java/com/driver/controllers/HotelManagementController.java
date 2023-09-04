90c327a2552384ecd3d0179300b69b00e68417d4
Vikalpwebtech <vikalprautela@gmail.com>
2023-09-04 19:52:54 +0530

    Final Submission

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -130,6 +130,7 @@ public class HotelManagementController {
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
         //Note that newFacilities can also have duplicate facilities possible
+        //checked
 
         List<Facility> facilities1 = hoteldb.get(hotelName).getFacilities();
 
